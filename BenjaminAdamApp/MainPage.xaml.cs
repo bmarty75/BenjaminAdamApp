815570aa2bbdbfa530785768857cc67b103df2ac
Adam <adamsouames08@gmail.com>
2026-03-24 11:35:55 +0100

    feat: animation d'entrée du logo au démarrage

--- BenjaminAdamApp/MainPage.xaml.cs
+++ BenjaminAdamApp/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace BenjaminAdamApp;
+namespace BenjaminAdamApp;
 
 public partial class MainPage : ContentPage
 {
@@ -7,5 +7,17 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
-	
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		LogoImage.Scale = 0;
+		await LogoImage.ScaleTo(1.2, 500, Easing.SpringOut);
+		await LogoImage.ScaleTo(1, 250, Easing.Linear);
+	}
+
+	private async void OnGifButtonClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new GifPage());
+	}
 }

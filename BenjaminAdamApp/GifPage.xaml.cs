fffb78978843fcb66f86e6efed8729cb9517eff2
Adam <adamsouames08@gmail.com>
2026-03-24 11:50:25 +0100

    feat: bouton et page GIF avec retour accueil

---
+++ BenjaminAdamApp/GifPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace BenjaminAdamApp;
+
+public partial class GifPage : ContentPage
+{
+	public GifPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}

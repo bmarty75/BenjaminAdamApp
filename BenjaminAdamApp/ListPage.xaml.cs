db52da6dbed1144336e9a0f575b9d3447b85a675
Adam <adamsouames08@gmail.com>
2026-03-25 11:11:50 +0100

    implémentation MVVM avec ListViewModel pour afficherles bières depuisAPI

--- BenjaminAdamApp/ListPage.xaml.cs
+++ BenjaminAdamApp/ListPage.xaml.cs
@@ -2,27 +2,17 @@ namespace BenjaminAdamApp;
 
 public partial class ListPage : ContentPage
 {
-    private BeerService _service = new BeerService();
+    private readonly ListViewModel _viewModel = new ListViewModel();
 
     public ListPage()
     {
         InitializeComponent();
+        BindingContext = _viewModel;
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-    
-        // lancement du chargement
-        LoadingIndicator.IsRunning = true;
-        LoadingIndicator.IsVisible = true;
-
-        var bieres = await _service.GetBeersAsync();
-        
-        BeerCollection.ItemsSource = bieres;
-        
-        BeerCollection.IsVisible = true;
-        LoadingIndicator.IsVisible = false;
-        LoadingIndicator.IsRunning = false;
+        await _viewModel.LoadBeersAsync();
     }
 }
---
+++ BenjaminAdamApp/ListViewModel.cs
@@ -0,0 +1,63 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace BenjaminAdamApp;
+
+public class ListViewModel : INotifyPropertyChanged
+{
+    private readonly BeerService _service = new BeerService();
+
+    public ObservableCollection<Beer> Beers { get; } = new ObservableCollection<Beer>();
+
+    private bool _isLoading = true;
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set { _isLoading = value; OnPropertyChanged(); }
+    }
+
+    private bool _isListVisible = false;
+    public bool IsListVisible
+    {
+        get => _isListVisible;
+        set { _isListVisible = value; OnPropertyChanged(); }
+    }
+
+    public ICommand LoadBeersCommand { get; }
+    public ICommand SelectBeerCommand { get; }
+
+    public ListViewModel()
+    {
+        LoadBeersCommand = new Command(async () => await LoadBeersAsync());
+        SelectBeerCommand = new Command<Beer>(async (beer) => await OnBeerSelected(beer));
+    }
+
+    public async Task LoadBeersAsync()
+    {
+        IsLoading = true;
+        IsListVisible = false;
+
+        var list = await _service.GetBeersAsync();
+        Beers.Clear();
+        foreach (var beer in list)
+            Beers.Add(beer);
+
+        IsLoading = false;
+        IsListVisible = true;
+    }
+
+    private async Task OnBeerSelected(Beer beer)
+    {
+        if (beer == null) return;
+        await Shell.Current.GoToAsync("beerdetail", new Dictionary<string, object>
+        {
+            { "Beer", beer }
+        });
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
